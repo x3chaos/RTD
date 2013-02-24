8ebb1ce3fbf5bb2c4db209b78b07551dd8e4e8a3
x3chaos <x3chaos@gmail.com>
2013-02-23 22:48:37 -0600

    Implement better testing
    Add batch and Python files for better testing. Files build environment
    by ensuring that the ./server/plugins folder is clean as if the plugin
    had just been installed.

---
+++ test.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set serverloc="./server/"
+set craftbukkit="craftbukkit.jar"
+
+:BUILD
+echo Initializing test server...
+python test.py
+if %errorlevel%==0 goto SUCCESS else goto FAILURE
+
+:FAILURE
+echo.
+pause
+goto :eof
+
+:SUCCESS
+echo Done.
+echo.
+echo Starting test server...
+echo.
+cd %serverloc%
+./run.bat %craftbukkit%
+if %errorlevel%==0 goto :eof else goto FAILURE
